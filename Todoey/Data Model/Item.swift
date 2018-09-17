88a3b113bf2e049e75fa845c9af740f7b61642c4
Edgar Bekier <edgar.bekier@devinci.fr>
2018-09-17 22:08:12 +0200

    Convert to MVC design pattern.

---
+++ Todoey/Data Model/Item.swift
@@ -0,0 +1,15 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by Edgar Bekier on 17/09/2018.
+//  Copyright © 2018 Edgar Bekier. All rights reserved.
+//
+
+import Foundation
+
+
+class Item {
+    var title : String = ""
+    var done : Bool = false
+}
